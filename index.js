3956859891d9090e7864267aba21a46ba7b02de2
Jens Andresen <jensandresen@gmail.com>
2017-11-29 20:27:56 +0100

    refactor index.js and added more help commands

--- index.js
+++ index.js
@@ -1,12 +1,59 @@
 const path = require("path");
+const fs = require("fs");
 const yamlFile = require("yamlfile");
 const padright = require("pad-right");
-
+const argsParser = require("minimist");
 const RunnerService = require("./runnerservice.js");
 
-const composeFileName = "./service-compose.yaml";
-const data = yamlFile.readFileSync(composeFileName);
 
+const args = argsParser(process.argv.slice(2));
+
+function printHelp() {
+    console.log("Usage:");
+    console.log("  service-compose [-f|--file <filename>]")
+    console.log("  service-compose --version")
+    console.log("  service-compose --help")
+    console.log("")
+    console.log("Options:")
+    console.log("  -f, --file <filename>    Specify a compose file (default: service-compose.yml)")
+    console.log("  --version                Shows the version of the current installation");
+    console.log("  --help                   Shows this help information");
+    console.log("");
+}
+
+if (args._.length == 0 && args.help === true) {
+    printHelp();
+    process.exit(0);
+}
+
+if (args._.length == 0 && args.version === true) {
+    const info = require("./package.json");
+    console.log(`Current version is v${info.version}`);
+    process.exit(0);
+}
+
+let filename = "service-compose.yml";
+
+if (process.argv.slice(2).length == 0 || args.f || args.file) {
+    filename = args.f || args.file || "service-compose.yml";
+    
+    if (filename === true) {
+        console.log("Missing filename from the invocation. Try 'service-compose --help' for more information.")
+        process.exit(1);
+    }
+} else {
+    console.log("Invalid invocation!")
+    console.log("")
+    printHelp();
+    process.exit(1);
+}
+
+if (!fs.existsSync(filename)) {
+    console.log(`Unable to locate the compose file: ${filename}`);
+    process.exit(1);
+}
+
+const data = yamlFile.readFileSync(filename);
 
 function convertToArray(obj) {
     const result = new Array();
@@ -19,7 +66,6 @@ function convertToArray(obj) {
     }
     return result;
 }
-
 
 
 const components = convertToArray(data.components);
